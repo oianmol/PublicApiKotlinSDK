77357a9e5850c10bc1d57904583426bfe938f549
Anmol Verma <anmol.verma4@gmail.com>
2023-08-19 21:06:35 +0530

    add: example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,18 @@ import dev.baseio.videoimpl.DownloadRequest
 import dev.baseio.videoimpl.VideoDownloader
 import dev.baseio.videoimpl.VideoDownloaderProvider
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>) = runBlocking {
+fun main() = runBlocking {
     val downloader: VideoDownloader = VideoDownloaderProvider.provider()
     with(downloader) {
-        val flow = download(DownloadRequest("5", "https://netflix.com/file.mp4"))
+        val flow = download(
+            DownloadRequest(
+                user = "5", // change user to see how the flow fails
+                fileUrl = "https://netflix.com/file.mp4"
+            )
+        )
         this@runBlocking.launch {
             flow.collect {
                 println(it)
