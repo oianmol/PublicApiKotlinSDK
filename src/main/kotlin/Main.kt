054464ed469b2459189626caa2f8c483787ba599
Anmol Verma <anmol.verma4@gmail.com>
2023-08-19 21:13:23 +0530

    add: example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,21 +7,21 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     val downloader: VideoDownloader = VideoDownloaderProvider.provider()
+    // downloader as ExpertVideoDownloader // fails since ExpertVideoDownloader is not visible
     with(downloader) {
-        val flow = download(
-            DownloadRequest(
-                user = "5", // change user to see how the flow fails
-                fileUrl = "https://netflix.com/file.mp4"
-            )
+        val request = DownloadRequest(
+            user = "5", // change user to see how the flow fails
+            fileUrl = "https://netflix.com/file.mp4"
         )
+        val flow = download(downloadRequest = request)
         this@runBlocking.launch {
             flow.collect {
                 println(it)
                 if (it.isDownloadComplete) {
                     cancel()
                 }
-                it.exception?.let {
-                    println(it)
+                it.exception?.let { throwable ->
+                    println(throwable)
                     cancel()
                 }
             }
